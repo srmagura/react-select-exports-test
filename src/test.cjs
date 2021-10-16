45e6c1a9d01c1dc7aadd022c701a243df5e1c1a6
Sam Magura <srmagura@gmail.com>
2021-10-16 17:09:48 -0400

    Add Node tests

---
+++ src/test.cjs
@@ -0,0 +1,13 @@
+const Select = require("react-select");
+const BaseSelect = require("react-select/base");
+const AnimatedSelect = require("react-select/animated");
+const AsyncSelect = require("react-select/async");
+const CreatableSelect = require("react-select/creatable");
+const AsyncCreatableSelect = require("react-select/async-creatable");
+
+console.log("Select:", Select);
+console.log("BaseSelect:", BaseSelect);
+console.log("AnimatedSelect:", AnimatedSelect);
+console.log("AsyncSelect:", AsyncSelect);
+console.log("CreatableSelect:", CreatableSelect);
+console.log("AsyncCreatableSelect:", AsyncCreatableSelect);
---
+++ src/test.mjs
@@ -0,0 +1,13 @@
+import Select from "react-select";
+import BaseSelect from "react-select/base";
+import AnimatedSelect from "react-select/animated";
+import AsyncSelect from "react-select/async";
+import CreatableSelect from "react-select/creatable";
+import AsyncCreatableSelect from "react-select/async-creatable";
+
+console.log("Select:", Select);
+console.log("BaseSelect:", BaseSelect);
+console.log("AnimatedSelect:", AnimatedSelect);
+console.log("AsyncSelect:", AsyncSelect);
+console.log("CreatableSelect:", CreatableSelect);
+console.log("AsyncCreatableSelect:", AsyncCreatableSelect);
